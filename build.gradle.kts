d102759f81a02cb218e313a6d39f98148a9373e3
Noah <noah.uyttebroeck@gmail.com>
2025-06-10 12:55:46 +0200

    Publishing support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,52 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java")
+    id("signing")
+    id("com.vanniktech.maven.publish") version "0.32.0"
 }
 
 group = "io.github.nowipi"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+
+signing {
+    useInMemoryPgpKeys(
+        project.findProperty("signing.keyId") as String?,
+        project.findProperty("signing.secretKey") as String?,
+        project.findProperty("signing.password") as String?
+    )
+}
+
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("io.github.nowipi.jio", "jio", project.version.toString())
+
+    pom {
+        name.set("JIO")
+        description.set("An extension of Java NIO")
+        url.set("https://github.com/Nowipi/JIO/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("Nowipi")
+                name.set("Noah Uyttebroeck")
+                url.set("https://github.com/Nowipi/")
+            }
+        }
+        scm {
+            url.set("https://github.com/Nowipi/JIO/")
+            connection.set("scm:git:git://github.com/Nowipi/jio.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Nowipi/jio.git")
+        }
+    }
+}
